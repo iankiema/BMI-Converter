f441768325e347f539a53bbed5b76f091d3ecc37
Ian <kiemaian.ik@gmail.com>
2023-02-13 17:44:39 +0300

    update

---
+++ index.js
@@ -0,0 +1,25 @@
+const btnEl =  document.getElementById("btn");
+const bmiInputEl = document.getElementById("bmi-result")
+const weightConditionEl = document.getElementById("weight-condition")
+const heightValue = document.getElementById("height").value / 100;
+const weightValue = document.getElementById("weight").value;
+
+function calculateBMI(){
+  
+  const bmiValue = weightValue / (heightValue * heightValue);
+  bmiInputEl.value = bmiValue;
+
+  if(bmiValue < 18.5){
+    weightConditionEl.innerText = "Under Weight"
+  }else if (bmiValue >= 18.5 && bmiValue <= 24.9){
+    weightConditionEl.innerText = "Normal Weight"
+  }else if (bmiValue >= 25 && bmiValue <=29.9){
+    weightConditionEl.innerText = "Overweight"
+  }else if(bmiValue >=30 ){
+    weightConditionEl.innerText = "Obesity"
+  }
+  
+}
+
+
+btnEl.addEventListener("click",calculateBMI);
